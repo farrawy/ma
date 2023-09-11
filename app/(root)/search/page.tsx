44030ff627e74ee00286bdfafaaf039000a70596
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2023-09-11 23:42:28 +0300

    First Commit

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,53 @@
+import UserCard from '@/components/cards/UserCard';
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+
+import { profileTabs } from '@/constants';
+
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+  });
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+      <div className="mt-3 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result ">No users</p>
+        ) : (
+          <>
+            {result.users.map((user) => (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                username={user.username}
+                imgUrl={user.image}
+                userType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Page;
---
+++ components/ui/index.ts
@@ -0,0 +1,35 @@
+'use client';
+import { Button } from './button';
+import { Input } from './input';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from './form';
+import { Label } from './label';
+import { Textarea } from './textarea';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';
+
+export {
+  Button,
+  Input,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+  Label,
+  Textarea,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+};
